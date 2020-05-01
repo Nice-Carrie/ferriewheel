48757e69e9dfff5f45beb9923050f8624662f7ef
Carrie LIn <cherrien@hotmail.com>
2020-05-01 17:56:44 +0800

    Initial Commit

---
+++ ferriewheel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ferriewheel
+//
+//  Created by Carrie(hinet) on 2020/5/1.
+//  Copyright © 2020 Carrie(hinet). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
